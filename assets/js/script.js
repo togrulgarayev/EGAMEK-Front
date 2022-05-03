b9f98886fbb969583b3671f3e318826537558d5a
Toğrul Qarazadə <garazade.tg@gmail.com>
2022-05-03 20:56:05 +0400

    create and fix user profile design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Game Carousel
 $(document).ready(function (){
     $('.game-carousel').owlCarousel({
         loop:true,
@@ -34,3 +35,4 @@ $(document).ready(function (){
         }
     });
 })
+// Game Carousel - End
